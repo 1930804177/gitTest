77deaad5bdd0685faf1b1c09f63f04eb7b8ee6d4
liwei <liwei@paraview.cn>
2019-12-23 17:34:57 +0800

    GitTest

---
+++ git-test-project/src/com/will/GitTest.java
@@ -0,0 +1,15 @@
+package com.will;
+
+/**
+ * @author Will_Lee
+ * @date 2019/12/23 - 17:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("will_lee");
+        System.out.println("will_lee");
+        System.out.println("will_lee");
+        System.out.println("will_lee");
+        System.out.println("will_lee");
+    }
+}
